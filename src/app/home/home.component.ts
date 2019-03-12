99a87306435fd722ad7b2c44183cb629d1013352
KatlegoTheKat <kg309@hotmail.com>
2019-03-13 01:53:21 +0200

    added a initializations for the service call

---
+++ src/app/home/home.component.ts
@@ -0,0 +1,60 @@
+import { Component, OnInit } from '@angular/core';
+import axios from 'axios';
+@Component({
+  selector: 'home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css']
+})
+export class HomeComponent implements OnInit {
+
+  locationDescription: string;
+  locationDate: string;
+  imperial: any;
+  metric: any;
+  weatherText: any;
+  province: string; 
+  country: string;
+  locationType: string;
+  countryID: string;
+  constructor() { }
+  
+  //A function to handle input searches
+  onSearch(form)
+  {
+	
+		
+		let locationDetails = {
+
+			locationName: form.value.locationName
+
+		}
+		console.log(locationDetails);
+		
+		axios.post("https://service1-weather-app.herokuapp.com/service1", locationDetails)
+		 .then(response =>{
+		 
+		    form.reset();
+			console.log(response.data);
+		     this.locationDescription = response.data.locationDetails.LocalizedName;
+			 this.locationDate = response.data.locationCondition.LocalObservationDateTime;
+			 this.province =  response.data.locationDetails.AdministrativeArea.LocalizedName;
+			 this.country = response.data.locationDetails.Country.LocalizedName;
+			 this.weatherText = response.data.locationCondition.WeatherText;
+			 this.metric = response.data.locationCondition.Temperature.Metric;
+			 this.imperial = response.data.locationCondition.Temperature.Imperial;
+			 this.locationType = response.data.locationDetails.Type;
+			 this.countryID =  response.data.locationDetails.Country.ID;
+			 
+			 
+		 }).catch(error =>{
+			
+			console.log(error);
+		 
+		 })
+  
+  }
+
+  ngOnInit() {
+  }
+
+}
